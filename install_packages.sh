f8af9e0751f087107badad27d983802487fb8411
Tom Softreck <info@softreck.dev>
2025-05-14 12:44:29 +0200

    update

--- install_packages.sh
+++ install_packages.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Script to install all pyfunc2 packages locally
-echo "Installing pyfunc2 packages..."
+# Script to install all pyfunc3 packages locally
+echo "Installing pyfunc3 packages..."
 
 # Install the file package
-echo "Installing pyfunc2-file..."
-pip install /home/tom/softreck/cfo/pyfunc2/file/dist/pyfunc2_file-0.1.0-py3-none-any.whl
+echo "Installing pyfunc3-file..."
+pip install /home/tom/softreck/cfo/pyfunc3/file/dist/pyfunc3_file-0.1.0-py3-none-any.whl
 
 # Install the config package
-echo "Installing pyfunc2-config..."
-pip install /home/tom/softreck/cfo/pyfunc2/config/dist/pyfunc2_config-0.1.0-py3-none-any.whl
+echo "Installing pyfunc3-config..."
+pip install /home/tom/softreck/cfo/pyfunc3/config/dist/pyfunc3_config-0.1.0-py3-none-any.whl
 
 # Install the email package
-echo "Installing pyfunc2-email..."
-pip install /home/tom/softreck/cfo/pyfunc2/email/dist/pyfunc2_email-0.1.0-py3-none-any.whl
+echo "Installing pyfunc3-email..."
+pip install /home/tom/softreck/cfo/pyfunc3/email/dist/pyfunc3_email-0.1.0-py3-none-any.whl
 
 # Install the ocr package
-echo "Installing pyfunc2-ocr..."
-pip install /home/tom/softreck/cfo/pyfunc2/ocr/dist/pyfunc2_ocr-0.1.0-py3-none-any.whl
+echo "Installing pyfunc3-ocr..."
+pip install /home/tom/softreck/cfo/pyfunc3/ocr/dist/pyfunc3_ocr-0.1.0-py3-none-any.whl
 
 # Install the main package
-echo "Installing pyfunc2 main package..."
-pip install /home/tom/softreck/cfo/pyfunc2/dist/pyfunc2-0.1.0-py3-none-any.whl
+echo "Installing pyfunc3 main package..."
+pip install /home/tom/softreck/cfo/pyfunc3/dist/pyfunc3-0.1.0-py3-none-any.whl
 
 echo "All packages installed successfully!"
